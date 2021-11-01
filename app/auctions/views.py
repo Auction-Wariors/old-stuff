19e7457cde6afe7bf43f841ac4da0cb83500ae75
sommerseth10 <fredrik17@gmail.com>
2021-11-01 22:45:09 +0100

    Updated auctions/views.py

--- app/auctions/views.py
+++ app/auctions/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from auctions.forms import AddAuctionForm, AddItemForm
 
 
 def view_auction(request):
@@ -6,4 +9,15 @@ def view_auction(request):
 
 
 def add_auction(request):
-    pass
+    if request.method == 'POST':
+        a_form = AddAuctionForm(request.POST)
+        i_form = AddItemForm(request.POST)
+        if i_form.is_valid() and a_form.is_valid():
+            #  FIXME: ADD LOGIC
+            messages.success(request, f'Auction added')
+            return redirect('stores:view_store')
+
+    else:
+        a_form = AddAuctionForm()
+        i_form = AddItemForm()
+    return render(request, 'auctions/add_auction.html', {'a_form': a_form, 'i_form': i_form})
