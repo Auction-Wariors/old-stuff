80b774b372834dbdf86b928c5d4cd4a25d145747
sommerseth10 <fredrik17@gmail.com>
2021-11-01 21:29:31 +0100

    Added forms.py to auctions app

---
+++ app/auctions/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from models import Item, Auction, Category
+
+
+class AddItemForm(forms.ModelForm):
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=True)
+
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'category']
+
+
+class AddAuctionForm(forms.ModelForm):
+    # FIXME: Need to change both start_date and end_date, use one of these?
+    #  - https://simpleisbetterthancomplex.com/tutorial/2019/01/03/how-to-use-date-picker-with-django.html
+    start_date = forms.DateTimeField(label='Start date')
+    end_date = forms.DateTimeField(label='End date')
+    min_price = forms.IntegerField(label='Minimum start bid')
+
+    class Meta:
+        model = Auction
+        fields = ['start_date', 'end_date', 'min_price']
