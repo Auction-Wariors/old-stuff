22b5ce264ac7779116e371698cd47c97148a4a2c
Sverre Larsen <sverre.christofferfotball@hotmail.com>
2021-11-02 14:58:38 +0100

    Added a TODO in stores/urls.py

--- app/stores/urls.py
+++ app/stores/urls.py
@@ -25,8 +25,9 @@ urlpatterns = [
     # "stores/"
     path('', views.view_all_stores, name='index'),
     path('create/', views.create_store, name='create_store'),
+    # TODO remove this when dynamic id on view_store path is in place
     path('1/', views.view_store, name='view_store'),
-    # TODO fix dynamic id on view_store path. Remove testing path
+    # TODO fix dynamic id on view_store path.
     # path('<int:store_id>/', views.view_store, name='view_store'),
     path('<int:store_id>/auctions/create', views.add_auction, name='add_auction'),  # hmm .... rethink this ?
 ]
