ab33c1427cba6a9b46b8d6827c708d9d2cd30170
sommerseth10 <fredrik17@gmail.com>
2021-11-02 17:39:24 +0100

    Added user registration. Not completed yet.

--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account successfully created for user: {username}')
+            return redirect('base:login')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 
 def user_profile(request):
