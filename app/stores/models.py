8b84daf42952aba2d26b35c43089d94e83aa3228
Anders <anders@iznogood.tech>
2021-11-04 20:11:25 +0100

    null has no effect on ManyToManyField.

--- app/stores/models.py
+++ app/stores/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Store(models.Model):
     name = models.CharField(max_length=100, default='Storename')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    moderators = models.ManyToManyField(User, related_name='moderator', blank=True, null=True)
+    moderators = models.ManyToManyField(User, related_name='moderator', blank=True)
     description = models.CharField(max_length=1000, default='')
     email = models.EmailField(default='')
     phone_number = models.CharField(max_length=12, default='')
