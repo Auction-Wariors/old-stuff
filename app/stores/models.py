8e2052bf8c0577b9ff43ef4f9a25fd679e6de07a
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2021-11-07 02:36:19 +0100

    Added fixme for phone number

--- app/stores/models.py
+++ app/stores/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Store(models.Model):
+    # FIXME: enforce int/format of phone number
     name = models.CharField(max_length=100, default='Storename')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     moderators = models.ManyToManyField(User, related_name='moderator', blank=True)
