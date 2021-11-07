11f65c34e2ae00146d46420c98036db99c0f7b61
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2021-11-07 02:35:58 +0100

    Added required fields so users can create a store

--- app/stores/forms.py
+++ app/stores/forms.py
@@ -3,6 +3,13 @@ from .models import Store
 
 
 class CreateStoreForm(forms.ModelForm):
+    # TODO: Display name of the user creating the store in the form
+    # TODO: Let creator select moderator/manager based on email?
+
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField()
+    phone_number = forms.NumberInput()
 
     class Meta:
         model = Store
