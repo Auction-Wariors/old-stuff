29a088fa852e7df2afe0035b6ccb4a1a35998123
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2021-11-07 02:37:40 +0100

    Added Store form tests

--- app/stores/tests.py
+++ app/stores/tests.py
@@ -2,42 +2,68 @@
 from django.test import TestCase
 
 from django.contrib.auth import get_user_model
-from app.stores.models import Store
+from stores.models import Store
 
 # Create your tests here.
+from stores.forms import CreateStoreForm
 
 User = get_user_model()
-Store = Store.objects.create()
 
+# Testing stores.forms.py
 
 class TestStoresForms(TestCase):
 
-    # Setup for the stores\tests to run.
     def setUp(self):
-        user_a_password = 'testUserA'
-        self.user_a_password = user_a_password
-        self.user_a = User(username='testUserA', email='TestUserA@notemail.com', password=user_a_password)
-        self.store_a = Store(name='testStoreA', owner='user_a')
+        user_password = 'testPassword'
+        user_owner = User.objects.create(username="user_owner", password=user_password)
 
-    # Verifying that setup was completed, and test user created.
-    def test_verify_test_setup(self):
+        store_name = "test store"
+        store = Store.objects.create(name=store_name, owner=user_owner, description='hello',
+                                     email='not@email.com', phone_number='11112222')
 
-        # Test parameters
-        user_qs = User.objects.filter(username_iexact='testUserA')
-        user_exists = user_qs.exitsts() and user_qs.count == 1
-        store_qs = Store.objects.filter(name_iexact='testStoreA')
-        store_exists = store_qs.exitsts() and store_qs.count == 1
-        user_a = user_qs.first()
+    def test_create_valid_store(self):
 
-        # User checks
-        self.assertTrue(user_exists)
-        self.assertNotEqual(user_exists.count(), 0)
-        self.assertTrue(user_a.check_password(self.user_a_password))
+        # Setting test variables
+        full_form = CreateStoreForm(data={'name': 'test store 1',
+                                          'description': 'lorem ipsum',
+                                          'email': 'isnot@email.com',
+                                          'phone_number': '22223333'})
+        not_complete_form = CreateStoreForm(data={'name': '',
+                                          'description': 'lorem ipsum',
+                                          'email': 'isnot@email.com',
+                                          'phone_number': ''})
+        empty_form = CreateStoreForm(data={'name': '',
+                                          'description': '',
+                                          'email': '',
+                                          'phone_number': ''})
+        no_name_in_form = CreateStoreForm(data={'name': '',
+                                          'description': 'lorem ipsum',
+                                          'email': 'isnot@email.com',
+                                          'phone_number': '22223333'})
+        no_description_in_form = CreateStoreForm(data={'name': 'test store 1',
+                                          'description': '',
+                                          'email': 'isnot@email.com',
+                                          'phone_number': '22223333'})
+        no_email_in_form = CreateStoreForm(data={'name': 'test store 1',
+                                          'description': 'lorem ipsum',
+                                          'email': '',
+                                          'phone_number': '22223333'})
+        no_phone_number_in_form = CreateStoreForm(data={'name': 'test store 1',
+                                          'description': 'lorem ipsum',
+                                          'email': 'isnot@email.com',
+                                          'phone_number': ''})
+        wrong_format_email_in_form = CreateStoreForm(data={'name': 'test store 1',
+                                          'description': 'lorem ipsum',
+                                          'email': '123151423',
+                                          'phone_number': 'notPhoneNo'})
 
-        # Store checks
-        self.assertTrue(store_exists)
-        self.assertNotEqual(store_exists.count(), 0)
-        self.assertTrue(self.store_a.owner == user_a)
+        # Checking the different forms
+        self.assertTrue(full_form.is_valid())
+        self.assertEqual(len(empty_form.errors), 4)
+        self.assertFalse(not_complete_form.is_valid())
+        self.assertFalse(no_name_in_form.is_valid())
+        self.assertFalse(no_description_in_form.is_valid())
+        self.assertFalse(no_email_in_form.is_valid())
+        self.assertFalse(no_phone_number_in_form.is_valid())
+        self.assertFalse(wrong_format_email_in_form.is_valid())
 
-    def test_register_store_only_when_logged_in(self):
-        pass
