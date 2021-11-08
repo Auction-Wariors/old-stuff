4cc79590a3ce5525d574b14753aa5b77a6e0c147
Sverre Larsen <sverre.christofferfotball@hotmail.com>
2021-11-08 19:35:17 +0100

    Added a todo for register?

--- app/base/urls.py
+++ app/base/urls.py
@@ -24,7 +24,8 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register'),
+    # TODO IS this in use?
+    #  path('register', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='base/logout.html'), name='logout')
 ]
