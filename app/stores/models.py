57671da56a20b64445ef7a84ebedf82c24f8ebc2
Anders <anders@iznogood.tech>
2021-11-15 21:27:16 +0100

    removed moderators (limitations)

--- app/stores/models.py
+++ app/stores/models.py
@@ -6,7 +6,6 @@ class Store(models.Model):
     # FIXME: enforce int/format of phone number
     name = models.CharField(max_length=100, default='Storename')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    moderators = models.ManyToManyField(User, related_name='moderator', blank=True)
     description = models.CharField(max_length=1000, default='')
     email = models.EmailField(default='')
     phone_number = models.CharField(max_length=12, default='')
