61927d2174449fc8a1af39dbb3936c4bad03cddb
sommerseth10 <fredrik17@gmail.com>
2021-11-16 12:55:35 +0100

    ...

--- app/base/context_processors.py
+++ app/base/context_processors.py
@@ -5,6 +5,5 @@ def store(request):
     if request.user.is_anonymous:
         return {'store': 'test'}
     user_has_store = Store.objects.filter(owner=request.user).first()
-    owner = user_has_store
-    return {'store': owner}
+    return {'store': user_has_store}
 
