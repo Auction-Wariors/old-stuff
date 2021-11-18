e0058ac6d59e2d0623bde1368afe83bb8c7ece58
sommerseth10 <fredrik17@gmail.com>
2021-11-18 13:24:29 +0100

    Added won unpayed/payed auctions to profile page

--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
+from auctions.models import Auction
 from .models import Profile
 
 from users.forms import UserRegisterForm
@@ -25,4 +27,6 @@ def register(request):
 
 def user_profile(request):
     profile = Profile.objects.get(user=request.user)
-    return render(request, 'users/profile.html', {'profile': profile})
+    winning_auctions = Auction.objects.filter(winner=request.user, isPayed=False).order_by('end_date')
+    payed_auctions = Auction.objects.filter(winner=request.user, isPayed=True).order_by('end_date')
+    return render(request, 'users/profile.html', {'profile': profile, 'auctions': winning_auctions, 'payed_auctions': payed_auctions})
