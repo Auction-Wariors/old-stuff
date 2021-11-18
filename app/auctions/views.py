e6774545af9e02696099deb60c2d93efc379f5ee
sommerseth10 <fredrik17@gmail.com>
2021-11-18 15:52:35 +0100

    Cleaning up code

--- app/auctions/views.py
+++ app/auctions/views.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 from django.contrib import messages
 from stores.models import Store
 from .models import Auction, Bid
-import datetime
 
 from auctions.forms import AddAuctionForm
 
@@ -16,24 +15,8 @@ def auction_detail(request, pk):
     current_leading_value = bids.aggregate(Max('value'))
     current_leading_bid = Bid.objects.filter(auction=auction, value__exact=current_leading_value["value__max"]).first()
 
-    # Time left auction, create own function?
-    time = auction.end_date - timezone.now()
-    print(time)
-    time_left = time.total_seconds()
-
-    days = time_left // (24 * 3600)
-    time_left = time_left % (24 * 3600)
-    hours = time_left // 3600
-    time_left %= 3600
-    minutes = time_left // 60
-    time_left %= 60
-    seconds = time_left
-    count_down = {
-        'days': int(days),
-        'hours': int(hours),
-        'minutes': int(minutes),
-        'seconds': int(seconds)
-    }
+    # Extracted in own function
+    count_down = count_down_func(auction)
 
     if not current_leading_bid:
         leading_bid = auction.min_price - 1
@@ -41,10 +24,7 @@ def auction_detail(request, pk):
         leading_bid = current_leading_bid.value
 
     if request.method == 'POST' and 'bid_value' in request.POST:
-        bid = Bid()
-        bid.owner = request.user
-        bid.auction = auction
-        bid.value = int(request.POST['bid_value'])
+        bid = Bid(owner=request.user, auction=auction, value=int(request.POST['bid_value']))
         if bid.value <= leading_bid:
             messages.warning(request, "Bid is too low")
             # FIXME: Add javascript in template?
@@ -59,8 +39,6 @@ def auction_detail(request, pk):
             bid.auction.highest_bid = bid.value
             bid.auction.save()
             bid.save()
-            print(f"Bid added: {datetime.datetime.now()}")
-            print(f"Bid added: {timezone.now()}")
             return redirect('auctions:auction_detail', pk=pk)
 
     return render(request, 'auctions/auction_detail.html', {'auction': auction,
@@ -85,6 +63,26 @@ def add_auction(request):
     else:
         form = AddAuctionForm()
     return render(request, 'auctions/add_auction.html', {'form': form})
+
+
+def count_down_func(auction):
+    time = auction.end_date - timezone.now()
+    print(time)
+    time_left = time.total_seconds()
+    days = time_left // (24 * 3600)
+    time_left = time_left % (24 * 3600)
+    hours = time_left // 3600
+    time_left %= 3600
+    minutes = time_left // 60
+    time_left %= 60
+    seconds = time_left
+    count_down = {
+        'days': int(days),
+        'hours': int(hours),
+        'minutes': int(minutes),
+        'seconds': int(seconds)
+    }
+    return count_down
 
 
 # TEST
