187e1c21bc3095ab1f7bcf4090ce284a4aab4bbb
Anders <anders@iznogood.tech>
2021-11-21 18:22:09 +0100

    Changed Profile / User relationship
    A user can only have one Profile and Vice Versa therefor it's a OneToOne and not a OneToMany

--- app/users/models.py
+++ app/users/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=12, default='')
     street_address = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=100, default='')
     zip_code = models.CharField(max_length=5, default='')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f"{self.user.username}'s Profile"
