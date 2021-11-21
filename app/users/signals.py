bf0e3da10c9b85582be11096f3937e0e56dfb5ce
Anders <anders@iznogood.tech>
2021-11-21 18:20:29 +0100

    Cleaned code

--- app/users/signals.py
+++ app/users/signals.py
@@ -1,12 +1,18 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
 from .models import Profile
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=get_user_model())
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        first_name = instance.first_name
-        last_name = instance.last_name
-        Profile.objects.create(user=instance, first_name=first_name, last_name=last_name)
+        print('Created fired!')
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=get_user_model())
+def save_user_profile(sender, instance, **kwargs):
+    print('Save fired!')
+    instance.profile.save()
