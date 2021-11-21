2aa7c07d27e41c464600a8925aff67b60641fb8c
Anders <anders@iznogood.tech>
2021-11-21 21:33:52 +0100

    refactor Store model

--- app/stores/models.py
+++ app/stores/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Store(models.Model):
-    # FIXME: enforce int/format of phone number
-    name = models.CharField(max_length=100, default='Storename')
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    description = models.CharField(max_length=1000, default='')
-    email = models.EmailField(default='')
-    phone_number = models.CharField(max_length=12, default='')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=12)
 
     def __str__(self):
         return self.name
