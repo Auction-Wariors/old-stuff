e7e6aed72b708894e7b6fbec2abd73811ea6c3ac
Anders <anders@iznogood.tech>
2021-11-21 21:34:35 +0100

    remove related_name (wrong use) remove Cascade (keep history)

--- app/stores/models.py
+++ app/stores/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Store(models.Model):
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     description = models.CharField(max_length=1000)
     email = models.EmailField()
     phone_number = models.CharField(max_length=12)
