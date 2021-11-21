f822e3b2435d067890413998c8f4bf542c8a1008
Anders <anders@iznogood.tech>
2021-11-21 18:01:24 +0100

    Removed redundant first/last name in profile

--- app/users/models.py
+++ app/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=40, default='')
-    last_name = models.CharField(max_length=50, default='')
     phone_number = models.CharField(max_length=12, default='')
     street_address = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=100, default='')
