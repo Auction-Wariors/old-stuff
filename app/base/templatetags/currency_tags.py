48004d79b44bedbeed44bcaec516c1e279e1bd3d
Anders <anders@iznogood.tech>
2021-12-02 18:09:26 +0100

    added filter for default raise

--- app/base/templatetags/currency_tags.py
+++ app/base/templatetags/currency_tags.py
@@ -8,3 +8,12 @@ register = template.Library()
 def divide_by_100(value):
     return math.ceil(value / 100)
 
+
+@register.filter
+def global_raise_value(value):
+    """"
+    TODO: This should be a model or a function calculating the raise in bid's
+    TODO: i.e an auction lower than 1000 should raise by 50 and higher than by 100 ...
+    # No way of having a 2 line t o d o <-- "--><--"
+    """
+    return value + 5
