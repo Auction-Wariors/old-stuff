7910fcb6cffde36abeb540b6f691cbd289192d24
Anders <anders@iznogood.tech>
2021-12-03 17:27:07 +0100

    removed comments

--- app/auctions/models.py
+++ app/auctions/models.py
@@ -21,12 +21,12 @@ class Auction(models.Model):
     description = models.TextField(max_length=5000)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
-    is_active = models.BooleanField(default=True)  # TODO: Do we need this ???
+    is_active = models.BooleanField(default=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     highest_bid = models.IntegerField(null=True, blank=True)
     winner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-    isPayed = models.BooleanField(default=False)  # TODO: Do we need this ???
+    isPayed = models.BooleanField(default=False)
     min_price = models.IntegerField()
 
     def __str__(self):
