8348031f54ae5d898de1fc9405b4744ccbc9eac3
sommerseth10 <fredrik17@gmail.com>
2021-12-04 18:21:54 +0100

    Bugfix TestUpdateStoreProfile

--- app/tests/views/test_stores_views.py
+++ app/tests/views/test_stores_views.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from http import HTTPStatus
-from django.utils import timezone
 from django.urls import reverse
-
-from auctions.models import Category, Auction, Bid
 from stores.models import Store
 
 
@@ -51,36 +48,37 @@ class TestCreateStore(TestCase):
         self.assertTemplateUsed(response, 'stores/create_store.html')
         self.assertContains(response, 'Register your Store', html=True)
 
-    class TestUpdateStoreProfile(TestCase):
-        """Testing Update Store Profile"""
 
-        @classmethod
-        def setUpTestData(cls):
-            user = User.objects.create(username='test')
-            user.set_password('test123')
-            user.save()
-            get_user = User.objects.get(username='test')
-            Store.objects.create(name='testStore',
-                                 owner=get_user,
-                                 description='test',
-                                 email='test@teststore.com',
-                                 phone_number='12345678')
+class TestUpdateStoreProfile(TestCase):
+    """Testing Update Store Profile"""
 
-        def test_update_store_success(self):
-            self.client.login(username='test_user', password='test123')
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='test_user')
+        user.set_password('test123')
+        user.save()
+        get_user = User.objects.get(username='test_user')
+        Store.objects.create(name='testStore',
+                             owner=get_user,
+                             description='test',
+                             email='test@teststore.com',
+                             phone_number='12345678')
 
-            response = self.client.post(reverse('stores:update_store'), data={'name': 'Test store updated',
-                                                                              'description': 'test updated',
-                                                                              'email': 'new@email.com',
-                                                                              'phone_number': '87654321'})
-            store_updated = Store.objects.get(name='Test store updated')
-            self.assertEqual(store_updated.name, 'Test store updated')
-            self.assertEqual(response.status_code, HTTPStatus.FOUND)
-            self.assertEqual(response["Location"], reverse('stores:store_dashboard'))
+    def test_update_store_success(self):
+        self.client.login(username='test_user', password='test123')
 
-        def test_update_store_get(self):
-            self.client.login(username='test_user', password='test123')
+        response = self.client.post(reverse('stores:update_store'), data={'name': 'Test store updated',
+                                                                          'description': 'test updated',
+                                                                          'email': 'new@email.com',
+                                                                          'phone_number': '87654321'})
+        store_updated = Store.objects.get(name='Test store updated')
+        self.assertEqual(store_updated.name, 'Test store updated')
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertEqual(response["Location"], reverse('stores:store_dashboard'))
 
-            response = self.client.get(reverse('stores:update_store'), follow=True)
-            self.assertTemplateUsed(response, 'stores/update_store.html')
-            self.assertContains(response, 'Update your store information', html=True)
+    def test_update_store_get(self):
+        self.client.login(username='test_user', password='test123')
+
+        response = self.client.get(reverse('stores:update_store'), follow=True)
+        self.assertTemplateUsed(response, 'stores/update_store.html')
+        self.assertContains(response, 'Update your store information', html=True)
