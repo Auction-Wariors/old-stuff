7f718888316740341c0a2d1f0d401cdfa0113f28
sommerseth10 <fredrik17@gmail.com>
2021-12-06 12:59:57 +0100

    Added auction to base view test

--- app/tests/views/test_base_views.py
+++ app/tests/views/test_base_views.py
@@ -8,36 +8,37 @@ from stores.models import Store
 
 
 class TestBaseViews(TestCase):
-    # set up test data for check_auction?
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='Test Category',
+                                           description='test description')
+        cls.category2 = Category.objects.create(name='Test Category 2',
+                                                description='test description')
+        user = User.objects.create(username='test_user')
+        cls.user_bid = User.objects.create(username='bid_user', password='bid_user_password')
+        user.set_password('test123')
+        user.save()
+        get_user = User.objects.get(username='test_user')
+        store = Store.objects.create(name='testStore',
+                                     owner=get_user,
+                                     description='test',
+                                     email='fr@ed.no',
+                                     phone_number='12345678')
 
-    # def setUpTestData(cls):
-    #     category = Category.objects.create(name='Test Category',
-    #                                        description='test description')
-    #     cls.category2 = Category.objects.create(name='Test Category 2',
-    #                                             description='test description')
-    #     user = User.objects.create(username='test_user')
-    #     cls.user_bid = User.objects.create(username='bid_user', password='bid_user_password')
-    #     user.set_password('test123')
-    #     user.save()
-    #     get_user = User.objects.get(username='test_user')
-    #     store = Store.objects.create(name='testStore',
-    #                                  owner=get_user,
-    #                                  description='test',
-    #                                  email='fr@ed.no',
-    #                                  phone_number='12345678')
-    #
-    #     cls.auction = Auction.objects.create(name='test auction',
-    #                                          description='test description',
-    #                                          category=category,
-    #                                          store=store,
-    #                                          is_active=True,
-    #                                          start_date=timezone.now(),
-    #                                          end_date=timezone.now() + timezone.timedelta(days=5),
-    #                                          min_price=200,
-    #                                          buy_now=300)
+        cls.auction = Auction.objects.create(name='test auction',
+                                             description='test description',
+                                             category=category,
+                                             store=store,
+                                             is_active=True,
+                                             start_date=timezone.now(),
+                                             end_date=timezone.now() + timezone.timedelta(days=5),
+                                             min_price=200,
+                                             buy_now=300)
 
     def test_index_get(self):
         response = self.client.get(reverse('base:index'))
+
+        self.assertContains(response, 'test auction', html=True)
         self.assertTemplateUsed(response, 'base/index.html')
 
     def test_pricing_page(self):
