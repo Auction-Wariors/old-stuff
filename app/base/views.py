0c7a1ffb3482aa8340f85c6ace891d9216d6be82
Anders <anders@iznogood.tech>
2021-12-07 12:43:06 +0100

    added automatic log in

--- app/base/views.py
+++ app/base/views.py
@@ -1,10 +1,35 @@
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render
+from django.contrib import messages
+
 from auctions.models import Auction, Category
 
 
 def index(request):
     auctions = Auction.objects.order_by('-start_date').filter(is_active=True)
     categories = Category.objects.all()
+    param_user = request.GET.get('user', '')
+
+    if param_user == 'StoreUser2':
+        logout(request)
+        user = authenticate(username='StoreUser2',
+                            password='StoreUser2')
+        login(request, user)
+        messages.success(request, 'StoreUser1 logged in! ')
+
+    if param_user == 'User3':
+        logout(request)
+        user = authenticate(username='User3',
+                            password='User3')
+        login(request, user)
+        messages.success(request, 'User3 logged in! ')
+
+    if param_user == 'User4':
+        logout(request)
+        user = authenticate(username='User4',
+                            password='User4')
+        login(request, user)
+        messages.success(request, 'User4 logged in! ')
 
     return render(request, 'base/index.html', {'auctions': auctions, 'categories': categories})
 
--- app/static/js/global.js
+++ app/static/js/global.js
@@ -5,3 +5,11 @@ setTimeout(() => {
     alert.parentNode.removeChild(alert);
   }
 }, 3000)
+
+// Remove second alert after 4 seconds
+setTimeout(() => {
+  const alert = document.getElementById("alert")
+  if (alert) {
+    alert.parentNode.removeChild(alert);
+  }
+}, 4000)
