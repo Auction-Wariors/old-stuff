fe69615debd3eb8f246b5b4c3cd3935d4763ac96
sommerseth10 <fredrik17@gmail.com>
2021-12-07 10:33:12 +0100

    Fixed password bug and add admin

--- app/base/management/commands/populate.py
+++ app/base/management/commands/populate.py
@@ -2,17 +2,36 @@ from django.contrib.auth.hashers import make_password
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
+from auctions.models import Category
+
+
+def add_admin():
+    try:
+        User.objects.get(username='admin')
+    except User.DoesNotExist:
+        admin = User.objects.create(username='admin', password=make_password('admin'))
+        admin.is_superuser = True
+        admin.is_staff = True
+        admin.is_active = True
+        admin.save()
+
 
 def add_users():
     for x in range(3):
         try:
             User.objects.get(username=f'User {x + 1}')
         except User.DoesNotExist:
-            user = User.objects.create(username=f'User {x + 1}')
-            user.set_password = make_password('pw2022')
+            user = User.objects.create(username=f'User {x + 1}', password=make_password('Software2021'))
+
+
+def add_categories():
+    for x in range(3):
+        Category.objects.get_or_create(name=f'Category {x+1}')
 
 
 class Command(BaseCommand):
     def handle(self, **options):
+        add_admin()
         add_users()
+        add_categories()
         print("Populate database completed...")
